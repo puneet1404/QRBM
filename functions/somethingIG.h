3a4b92be6bcf91dd14bd44873200c54328567665
puneet <puneetthedynamo14@gmail.com>
2025-06-13 16:46:45 +0530

    Update somethingIG.h

--- functions/somethingIG.h
+++ functions/somethingIG.h
@@ -14,19 +14,44 @@ typedef arma::mat matrix;
 namespace neural_net
 {
 
+	// a class to simmulate the nodes(number is alpha) and the input 1d chain is column
 	class Neural_net
 	{
 	private:
+	//this is the matrix which is multiplied to the column vector for the node values to be obtained 
 		vector<matrix> nodes;
 
 		double B_value[alpha];
-		matrix Hidden_layer_init()
+		matrix Hidden_layer_init();
+	public:
+	Neural_net();
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//implimentation
+	matrix neural_net::Neural_net::Hidden_layer_init()
 		{
 			matrix Hidden_layer = arma::randu(alpha,columns);
 			return Hidden_layer;
 		}
 
-	public:
-	};
-}
 #endif

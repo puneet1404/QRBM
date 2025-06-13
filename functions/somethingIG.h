943b072bec9550f9fdcf28a1a7301a09f52a5301
puneet <puneetthedynamo14@gmail.com>
2025-06-13 23:59:01 +0530

    psi() calc added

--- functions/somethingIG.h
+++ functions/somethingIG.h
@@ -1,66 +1,63 @@
+#ifndef _somethingig_H_
+#define _somethingig_H_
 
-#ifndef _somethinglG_H_
-#define _somethinglG_H_
-#include<eigen3/Eigen/Dense>
-#include<vector>
-#include<iostream>
+#include <armadillo>
+#include <vector>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-const int rows=1;
-const int columns =5;
-const int alpha= 4;
+const int rows = 1;
+const int columns = 5;
+const int alpha = 4;
 
-class Hidden_Layer
+typedef arma::mat matrix;
+namespace neural_net
 {
-private:
-	vector<Eigen::Matrix<double, columns, rows>> nodes;
-	double B_value[alpha];
-	Eigen::MatrixXd Hidden_layer_init()
+	// just a thing to call visible layer
+	struct visible_layer
 	{
-		Eigen::MatrixXd Hidden_layer = Eigen::MatrixXd::Random(columns, rows);
-		return Hidden_layer;
-	}
+		matrix vis_lay = arma::randu(columns, 1);
+	};
+	// a class to simmulate the nodes(number is alpha) and the input 1d chain is column
+	class Neural_net
+	{
+	private:
+		// this is the matrix which is multiplied to the column vector for the node values to be obtained
+		void Hidden_layer_init();
 
-public:
-	Hidden_Layer()
-	{
-		for (int i = 0; i < alpha; i++)
-		{
-			nodes.push_back(Hidden_layer_init());
-			B_value[i] = rand() / RAND_MAX;
-		}
-	}
-	Eigen::Matrix<double, columns, rows> &operator()(int i)
-	{
-		return nodes[i];
-	}
-};
-class Visible_Layer
+	public:
+		double E_loc();
+		visible_layer Vis_lay;
+		double psi_s();
+		matrix b = arma::zeros(alpha, 1);
+		matrix hidden_layer;
+		Neural_net();
+	};
+}
+// implimentation
+void neural_net::Neural_net::Hidden_layer_init()
 {
-private:
-	Eigen::Matrix<double, rows, columns> layer;
+	hidden_layer = arma::randu(alpha, columns);
+	b = arma::randu(b.n_rows, b.n_cols);
+	Vis_lay.vis_lay.for_each([](matrix::elem_type &m)
+							 { (m > 0.5) ? (m = -1) : (m = 1); });
+	psi_s();
+}
 
-public:
-	Visible_Layer()
+double neural_net::Neural_net::psi_s()
+{
+	matrix m = hidden_layer * Vis_lay.vis_lay + b;
+	double psi = 1;
+	for (auto i : m)
 	{
-		layer = Eigen::MatrixXd::Random(rows, columns);
+		psi = cosh(i) * psi;
+	}
+	return psi;
+}
+neural_net::Neural_net::Neural_net()
+{
+	Hidden_layer_init();
+}
 
-		for (auto x : layer.rowwise())
-		{
-			for (auto y = x.begin(); y < x.end(); y++)
-			{
-				(*y > 0.5) ? (*y = 1) : (*y = -1);
-			}
-		}
-	}
-	double &operator()(int i, int j)
-	{
-		return layer(i, j);
-	}
-
-	friend ostream &operator<<(std::ostream &os, Visible_Layer const &m)
-	{
-		return os << m.layer;
-	}
-};
 #endif
